7a2c48f6c1a3c6c09595019615fbefe97e427cb4
durgarantnam <durgachilukuri02@gmail.com>
2024-08-28 16:21:38 +0100

    functions

---
+++ 09-functions.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+VALIDATE(){
+    echo "exit status: $1"
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "please run this priveleg"
+    exit 1
+fi
+
+dnf list installed git
+
+VALIDATE $?
+
+if [ $? -ne 0 ]
+then
+   echo "Git is not installed,going to install it.."
+   dnf install git -y
+   if [ $? -ne 0 ]
+   then
+       echo "git installation is not success...check it"
+       exit 1
+   else
+       echo "git istallation is success"
+   fi
+else
+    echo "Git is already installed,nothing to do.."    
+fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then
+   echo "Mysql is not installed,going to install it.."
+   dnf install mysql -y
+   if [ $? -ne 0 ]
+   then 
+      echo "mysql installation is not success...check it"
+      exit 1
+   else
+      echo "mysql istallation is success"
+    fi
+else
+    echo "mysql is already installed,nothing to do.."
+fi         
