daca77ff63b0067d23d7cab77b7d72938f860ffd
durgarantnam <durgachilukuri02@gmail.com>
2024-08-28 17:11:59 +0100

    functions

--- 09-functions.sh
+++ 09-functions.sh
@@ -20,36 +20,22 @@ fi
 
 dnf list installed git
 
-VALIDATE $? "Listing git"
+if [ $? -ne 0 ]
+then
+   echo "Git is not installed,going to install it.."
+   dnf install git -y
+   VALIDATE $? "installing Git"
+else
+    echo "Git is already installed,nothing to do.."    
+fi
 
-# if [ $? -ne 0 ]
-# then
-#    echo "Git is not installed,going to install it.."
-#    dnf install git -y
-#    if [ $? -ne 0 ]
-#    then
-#        echo "git installation is not success...check it"
-#        exit 1
-#    else
-#        echo "git istallation is success"
-#    fi
-# else
-#     echo "Git is already installed,nothing to do.."    
-# fi
+dnf list installed mysql
 
-# dnf list installed mysql
-
-# if [ $? -ne 0 ]
-# then
-#    echo "Mysql is not installed,going to install it.."
-#    dnf install mysql -y
-#    if [ $? -ne 0 ]
-#    then 
-#       echo "mysql installation is not success...check it"
-#       exit 1
-#    else
-#       echo "mysql istallation is success"
-#     fi
-# else
-#     echo "mysql is already installed,nothing to do.."
-# fi         
+if [ $? -ne 0 ]
+then
+   echo "Mysql is not installed,going to install it.."
+   dnf install mysql -y
+   VALIDATE $? "installing Mysql"
+else
+    echo "mysql is already installed,nothing to do.."
+fi         
