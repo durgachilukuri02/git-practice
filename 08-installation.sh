95e21091f15e80e42853b842f73effce6802d088
durgarantnam <durgachilukuri02@gmail.com>
2024-08-28 15:25:39 +0100

    functions

---
+++ 08-installation.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if[ $USERID -ne 0 ]
+then
+    echo"please run this priveleg"
+    exit 1
+fi
+
+dnf list installed gittt
+
+if [ $? -ne 0 ]
+then
+   echo "Git is not installed,going to install it.."
+   dnf install gittt -y
+   if [ $? -ne 0 ]
+   then
+       echo "git installation is not success...check it"
+       exit 1
+   else
+       echo "git istallation is success"
+   fi
+else
+    echo "Git is already installed,nothing to do.."    
+fi
