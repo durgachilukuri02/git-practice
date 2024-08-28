b6b0f13f8d5573c932c09909880ebf9afa2c7101
durgarantnam <durgachilukuri02@gmail.com>
2024-08-28 16:24:56 +0100

    functions

--- 09-functions.sh
+++ 09-functions.sh
@@ -16,34 +16,34 @@ dnf list installed git
 
 VALIDATE $?
 
-if [ $? -ne 0 ]
-then
-   echo "Git is not installed,going to install it.."
-   dnf install git -y
-   if [ $? -ne 0 ]
-   then
-       echo "git installation is not success...check it"
-       exit 1
-   else
-       echo "git istallation is success"
-   fi
-else
-    echo "Git is already installed,nothing to do.."    
-fi
+# if [ $? -ne 0 ]
+# then
+#    echo "Git is not installed,going to install it.."
+#    dnf install git -y
+#    if [ $? -ne 0 ]
+#    then
+#        echo "git installation is not success...check it"
+#        exit 1
+#    else
+#        echo "git istallation is success"
+#    fi
+# else
+#     echo "Git is already installed,nothing to do.."    
+# fi
 
-dnf list installed mysql
+# dnf list installed mysql
 
-if [ $? -ne 0 ]
-then
-   echo "Mysql is not installed,going to install it.."
-   dnf install mysql -y
-   if [ $? -ne 0 ]
-   then 
-      echo "mysql installation is not success...check it"
-      exit 1
-   else
-      echo "mysql istallation is success"
-    fi
-else
-    echo "mysql is already installed,nothing to do.."
-fi         
+# if [ $? -ne 0 ]
+# then
+#    echo "Mysql is not installed,going to install it.."
+#    dnf install mysql -y
+#    if [ $? -ne 0 ]
+#    then 
+#       echo "mysql installation is not success...check it"
+#       exit 1
+#    else
+#       echo "mysql istallation is success"
+#     fi
+# else
+#     echo "mysql is already installed,nothing to do.."
+# fi         
