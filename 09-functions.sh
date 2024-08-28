e9e995b256342de3e740ab1c4784295a834e8494
durgarantnam <durgachilukuri02@gmail.com>
2024-08-28 17:20:09 +0100

    functions

--- 09-functions.sh
+++ 09-functions.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 USERID=$(id -u)
+CHECK_ROOT(){
 
+    if [ $USERID -ne 0 ]
+    then
+        echo "please run this priveleg"
+        exit 1
+    fi 
+}
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
@@ -12,11 +19,7 @@ VALIDATE(){
     fi      
 }
 
-if [ $USERID -ne 0 ]
-then
-    echo "please run this priveleg"
-    exit 1
-fi
+CHECK_ROOT
 
 dnf list installed git
 
